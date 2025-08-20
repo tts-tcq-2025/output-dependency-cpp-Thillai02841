117c08a5307c02551340c6e44937c3f005833beb
Thillai02841 <thillai240201@gmail.com>
2025-08-20 12:02:37 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -59,14 +59,30 @@ namespace WeatherSpace
         int Humidity() const override { return 50; }
         int WindSpeedKMPH() const override { return 60; }      // strong winds
     };
+    // string Report(const IWeatherSensor& sensor)
+    // {
+    //     int precipitation = sensor.Precipitation();
+    //     // precipitation < 20 is a sunny day
+    //     string report = "Sunny Day";
+
+    //     if (sensor.TemperatureInC() > 25)
+    //     {
+    //         if (precipitation >= 20 && precipitation < 60)
+    //             report = "Partly Cloudy";
+    //         else if (sensor.WindSpeedKMPH() > 50)
+    //             report = "Alert, Stormy with heavy rain";
+    //     }
+    //     return report;
+    // }
     string Report(const IWeatherSensor& sensor)
     {
         int precipitation = sensor.Precipitation();
-        // precipitation < 20 is a sunny day
         string report = "Sunny Day";
-
-        if (sensor.TemperatureInC() > 25)
-        {
+    
+        if (precipitation > 60 && sensor.WindSpeedKMPH() < 50) {
+            report = "Rainy";
+        }
+        else if (sensor.TemperatureInC() > 25) {
             if (precipitation >= 20 && precipitation < 60)
                 report = "Partly Cloudy";
             else if (sensor.WindSpeedKMPH() > 50)
